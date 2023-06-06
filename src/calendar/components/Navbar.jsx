aeae92369f81c34d9c72cf7fe3b8cebef9ac3a84
emmanuel <enmanuelmartinez89@yahoo.es>
2023-06-05 22:03:23 -0600

    first commit

---
+++ src/calendar/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4 px-" >
+      <span>emma</span>
+      
+    </div>
+  );
+};
